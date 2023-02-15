8c7aba52d6814b8b48a628876c033c17f473c94a
Christopher Wuni <christdam55@gmail.com>
2023-02-15 18:53:35 +0100

    fixed dependencies issues with fund module

--- src/fund/fund.module.ts
+++ src/fund/fund.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { NowPaymentService } from '../nowpayment/nowpayment.service';
 import { UserModule } from '../user/user.module';
 import { FundController } from './fund.controller';
 import { FundService } from './fund.service';
 
 @Module({
   controllers: [FundController],
-  providers: [FundService],
+  providers: [FundService, NowPaymentService],
   imports: [UserModule],
 })
 export class FundModule {}
